2ff39906909a9053e2131d679e2e5ebfa0e43885
brian_li <brian.li@sgs.com>
2024-06-13 08:58:44 +0800

    add dll and icon in mylib

---
+++ MyLib/TSC.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MyLib
+{
+    public partial class TSC
+    {
+        // import the DLL
+        [LibraryImport("TSCLIB.dll", EntryPoint = "about")]
+        private static partial int about();
+
+        // implement the method
+        public static int About() => about();
+    }
+}
